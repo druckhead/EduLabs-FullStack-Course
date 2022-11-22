52d613c17347f3635406c900dd5b1f12f277ab2f
druckhead <daniel32raz@gmail.com>
2022-11-22 22:33:01 +0200

    remove #TODO and duplicate variable

--- Lessons/Lesson-5/Homework/src/B5_List_Dictionary_Set/E4.py
+++ Lessons/Lesson-5/Homework/src/B5_List_Dictionary_Set/E4.py
@@ -1,30 +1,28 @@
 from pprint import pprint
 
-
 colors_0 = [
-    'red', 'blue', 'Purple','white'
+    'red', 'blue', 'Purple', 'white'
 ]
 
 colors_1 = [
-    'orange red', 'blue navy', 'BLUE pure','snow white', 'sky blue', 'pure purple',
-    'white cream', 'Eggshell white','Orchid purple', 'Medium blue', 'Egyptian blue', 'Neon blue'
+    'orange red', 'blue navy', 'BLUE pure', 'snow white', 'sky blue', 'pure purple',
+    'white cream', 'Eggshell white', 'Orchid purple', 'Medium blue', 'Egyptian blue', 'Neon blue'
 ]
 
 colors_2 = [
-    'red Crimson', 'Liberty blue', 'Purple pizzazz','white & Red', 'sky blue',
+    'red Crimson', 'Liberty blue', 'Purple pizzazz', 'white & Red', 'sky blue',
     'Pale purple', 'Orchid purple', 'BLUE pure'
 ]
+
 
 # Create a function that receives 3 lists and returns output
 # (choose the right single type) that will indicate which colors are derived from basic colors.
 # Basic colors are ones stored in colors_0. Others are derived colors.
 
-# Case insensitive
+# Case-insensitive
 
-# TODO need to do
 
-def derived_colors(l1: list[str], l2:list[str], l3: list[str]) -> set:
-    colors_set: set[str] = set()
+def derived_colors(l1: list[str], l2: list[str], l3: list[str]) -> set:
     colors_set: set[str] = set(l2).union(set(l3))
 
     derived_colors_set: set[str] = set()
