95eded352b65e5c6e85b7f3b5a88a33b317ef0da
druckhead <daniel32raz@gmail.com>
2022-12-13 13:04:38 +0200

    fix bugs & update customexceptions.py

--- Lessons/Lesson_10/src/Homework/C10/customexceptions.py
+++ Lessons/Lesson_10/src/Homework/C10/customexceptions.py
@@ -1,14 +1,19 @@
-class NotACsvFile(Exception):
-    pass
+
+class IncompatibleFileTypeError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+        super().__init__(self.msg)
 
 
-class NotATxtFile(Exception):
-    pass
-
-
-class NotAJsonFile(Exception):
-    pass
+class IncompatibleCsvHeaders(Exception):
+    def __init__(self, lhs_header, rhs_header):
+        self.lhs_header = lhs_header
+        self.rhs_header = rhs_header
+        self.msg = f"Error: lhs={self.lhs_header} and rhs={self.rhs_header} posses different headers"
+        super().__init__(self.msg)
 
 
 class InvalidFileExtension(Exception):
-    pass
+    def __init__(self, msg: str):
+        self.msg = msg
+        super().__init__(self.msg)
--- Lessons/Lesson_10/src/Homework/C10/textfile.py
+++ Lessons/Lesson_10/src/Homework/C10/textfile.py
@@ -1,4 +1,3 @@
-import csv
 from csv import DictWriter, DictReader
 import json
 import os.path
@@ -10,9 +9,11 @@ from customexceptions import *
 class TextFile(ABC):
     def __init__(self, fpath: str):
         if not os.path.exists(fpath):
-            raise FileNotFoundError
-        if os.path.splitext(fpath)[-1] != self._get_ext():
-            raise InvalidFileExtension
+            raise FileNotFoundError(f"Invalid Path.\n{fpath} Does not exist.")
+        ext = os.path.splitext(fpath)[-1]
+        if ext != self._get_ext():
+            raise InvalidFileExtension(f"{ext} is not a supported extension for {self.__class__.__name__}. "
+                                       f"Should be {self._get_ext()}")
 
         self._fpath = fpath
 
@@ -29,14 +30,21 @@ class TextFile(ABC):
         return content
 
     def _validate_concat_path(self, other):
+        if not isinstance(other, TextFile):
+            raise IncompatibleFileTypeError(f"File type: {type(other).__name__} not supported"
+                                            f"Should have ABC of {type(TextFile).__name__}")
+        other_path: str = other._fpath
         if not isinstance(other, type(self)):
-            raise Exception(f"{other} is not a {self._get_ext()} file")
+            raise IncompatibleFileTypeError(
+                f"Can not concat incompatible file types. "
+                f"{type(other).__name__}({other_path.split('/')[-1]}) is not a {type(self).__name__}"
+                f"({self._fpath.split('/')[-1]}) file")
 
         abspath = os.path.abspath(self._fpath)
         newfile_path = '_'.join([os.path.splitext(abspath)[0], os.path.abspath(other._fpath).split('/')[-1]])
 
         if os.path.exists(newfile_path):
-            raise FileExistsError
+            raise FileExistsError(f'path=\'{newfile_path}\' already exists')
 
         return newfile_path
 
@@ -55,10 +63,18 @@ class CsvFile(TextFile):
 
         self._delimiter = delimiter
 
-    def _get_ext(self):
+    @property
+    def fpath(self) -> str:
+        return self._fpath
+
+    @property
+    def delimiter(self) -> str:
+        return self._delimiter
+
+    def _get_ext(self) -> str:
         return ".csv"
 
-    def _get_delimiter(self):
+    def _get_delimiter(self) -> str:
         return self._delimiter
 
     def _get_specific_content(self, fh: TextIO) -> list[dict[str | Any]]:
@@ -74,10 +90,10 @@ class CsvFile(TextFile):
         reader = DictReader(fh, delimiter=self._delimiter)
         fieldnames = reader.fieldnames
         for row in reader:
-            rowdict = {}
+            row_dict = {}
             for fn in fieldnames:
-                rowdict[fn] = row[fn]
-            ls.append(rowdict)
+                row_dict[fn] = row[fn]
+            ls.append(row_dict)
 
     def get_num_rows(self):
         jsonfile = self.get_file_content()
@@ -91,8 +107,8 @@ class CsvFile(TextFile):
         return len(jsonfile[-1].keys())
 
     def get_row(self, row_num: int):
-        if row_num < 1 or row_num > self.get_num_rows() + 1:
-            raise IndexError()
+        if not (1 <= row_num <= self.get_num_rows() + 1):
+            raise IndexError(f"Index {row_num} is out of bounds. Expected: {1} <= row_num <= {self.get_num_rows() + 1}")
 
         with open(self._fpath, 'r') as csvfile:
             reader = DictReader(csvfile, delimiter=self._delimiter)
@@ -109,9 +125,9 @@ class CsvFile(TextFile):
 
         return ret_val
 
-    def get_col(self, col: int):
-        if col < 1 or col > self.get_num_cols():
-            raise IndexError()
+    def get_col(self, col_num: int):
+        if not (1 <= col_num <= self.get_num_cols()):
+            raise IndexError(f"Index {col_num} is out of bounds. Expected: {1} <= col_num <= {self.get_num_cols()}")
         ret_val: list = []
 
         with open(self._fpath, 'r') as csvfile:
@@ -119,41 +135,40 @@ class CsvFile(TextFile):
             fieldnames = reader.fieldnames
 
             for row in reader:
-                ret_val.append(row[fieldnames[col - 1]])
+                ret_val.append(row[fieldnames[col_num - 1]])
 
         return ret_val
 
-    def get_cell(self, row: int, col: int):
-        if row < 1 or row > self.get_num_rows():
-            raise IndexError()
-        if col < 1 or col > self.get_num_cols():
-            raise IndexError()
+    def get_cell(self, row_num: int, col_num: int):
+        if not (1 <= row_num <= self.get_num_rows() + 1):
+            raise IndexError(f"Index {row_num} is out of bounds. Expected: {1} <= row_num <= {self.get_num_rows() + 1}")
+        if not (1 <= col_num <= self.get_num_cols()):
+            raise IndexError(f"Index {col_num} is out of bounds. Expected: {1} <= col_num <= {self.get_num_cols()}")
 
         with open(self._fpath, 'r') as csvfile:
             reader = DictReader(csvfile, delimiter=self._delimiter)
             fieldnames = reader.fieldnames
 
-        curr_row = self.get_row(row)
-        ret_val = curr_row.get(fieldnames[col - 1])
+        curr_row = self.get_row(row_num)
+        ret_val = curr_row.get(fieldnames[col_num - 1])
 
         return ret_val
 
     def __add__(self, other):
-        if not isinstance(other, type(self)):
-            raise Exception(f"{other} is not a {self._get_ext()} file")
-
         newfile_name = self._validate_concat_path(other)
 
         with open(self._fpath, 'r') as lhs:
-            lhs_reader = DictReader(lhs, delimiter=self._delimiter)
+            lhs_reader = DictReader(lhs, delimiter=self.delimiter)
             lhs_fieldnames = lhs_reader.fieldnames
 
-        with open(other._fpath, 'r') as rhs:
-            rhs_reader = DictReader(rhs, delimiter=other._delimiter)
+        with open(other.fpath, 'r') as rhs:
+            if isinstance(other, CsvFile):
+                pass
+            rhs_reader = DictReader(rhs, delimiter=other.delimiter)
             rhs_fieldnames = rhs_reader.fieldnames
 
         if set(lhs_fieldnames) != set(rhs_fieldnames):
-            raise Exception
+            raise IncompatibleCsvHeaders(lhs_fieldnames, rhs_fieldnames)
 
         new_content = []
 
@@ -207,9 +222,6 @@ class TxtFile(TextFile):
         return words_len / self.get_num_words()
 
     def __add__(self, other):
-        if not isinstance(other, type(self)):
-            raise Exception(f"{other} is not a {self._get_ext()} file")
-
         new_fpath = self._validate_concat_path(other)
         concatenated_content = self.get_file_content() + other.get_file_content()
 
