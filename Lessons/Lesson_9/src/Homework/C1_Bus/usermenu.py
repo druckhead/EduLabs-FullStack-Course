60dd024406dea5bc6236c7d65542736b54737f48
druckhead <daniel32raz@gmail.com>
2022-12-14 12:38:35 +0200

    unite passengermenu & managermenu

---
+++ Lessons/Lesson_9/src/Homework/C1_Bus/usermenu.py
@@ -0,0 +1,35 @@
+from src.Homework.C1_Bus.bestbuscompany import BestBusCompany
+
+
+class PassengerMenu:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def search_route(bus_company: BestBusCompany):
+        pass
+
+    @staticmethod
+    def report_delay(bus_company: BestBusCompany):
+        pass
+
+
+class ManagerMenu:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add_route(bus_company: BestBusCompany):
+        pass
+
+    @staticmethod
+    def delete_route(bus_company: BestBusCompany):
+        pass
+
+    @staticmethod
+    def update_route(bus_company: BestBusCompany):
+        pass
+
+    @staticmethod
+    def add_scheduled_ride(bus_company: BestBusCompany):
+        pass
