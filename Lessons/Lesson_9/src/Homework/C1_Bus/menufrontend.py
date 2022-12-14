21bced5748792b5ede94d990648fb26a08fc785f
druckhead <daniel32raz@gmail.com>
2022-12-15 01:00:35 +0200

    slow down flow to allow reading of error messages

--- Lessons/Lesson_9/src/Homework/C1_Bus/menufrontend.py
+++ Lessons/Lesson_9/src/Homework/C1_Bus/menufrontend.py
@@ -1,6 +1,7 @@
 from menubackend import Menu
 from os import system
 from exceptions import *
+from time import sleep
 
 
 class MainMenu:
@@ -27,47 +28,68 @@ class MainMenu:
                                 break
                             except TooManyWrongPasswordAttemptsError as err1:
                                 print(err1)
+                                sleep(1.5)
                                 break
                             except InvalidPasswordError as err2:
                                 print(err2)
+                                sleep(0.25)
                                 continue
 
                         if main_menu.signed_in:
                             main_menu.init_manager_menu()
-                            system("clear")
-                            main_menu.display_manager_actions()
                             while True:
+                                system("clear")
+                                main_menu.display_manager_actions()
                                 actions_choice = input()
                                 match actions_choice:
                                     case '1':
+                                        # todo accept inputs and validate them
+                                        #  before sending them as params in the functions
                                         main_menu.user_menu.add_route()
                                     case '2':
+                                        # todo accept inputs and validate them before sending them as params in the functions
                                         main_menu.user_menu.delete_route()
                                     case '3':
+                                        # todo accept inputs and validate them before sending them as params in the functions
                                         main_menu.user_menu.update_route()
                                     case '4':
+                                        # todo accept inputs and validate them before sending them as params in the functions
                                         main_menu.user_menu.add_scheduled_ride()
                                     case '5':
+                                        # todo accept inputs and validate them before sending them as params in the functions
                                         main_menu.sign_out()
                                         break
+                                    case _:
+                                        print("Invalid input. Please Choose from the options provided.")
+                                        sleep(1)
+                                        continue
                     case '2':
                         main_menu.init_passenger_menu()
-                        system("clear")
-                        main_menu.display_passenger_actions()
                         while True:
+                            system("clear")
+                            main_menu.display_passenger_actions()
                             actions_choice = input()
                             match actions_choice:
                                 case '1':
+                                    # todo accept inputs and validate them before sending them as params in the functions
                                     main_menu.user_menu.search_route()
                                 case '2':
+                                    # todo accept inputs and validate them before sending them as params in the functions
                                     main_menu.user_menu.report_delay()
                                 case '3':
+                                    # todo accept inputs and validate them before sending them as params in the functions
                                     main_menu.sign_out()
                                     break
+                                case _:
+                                    print("Invalid input. Please Choose from the options provided.")
+                                    sleep(1)
+                                    continue
                     case '3':
                         # main_menu.quit()
                         break
                     case _:
+                        print("Invalid input. Please Choose from the options provided.")
+                        sleep(1)
                         continue
         except KeyboardInterrupt:
             # main_menu.quit()
