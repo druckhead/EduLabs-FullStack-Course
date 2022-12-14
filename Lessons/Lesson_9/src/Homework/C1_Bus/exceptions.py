9977227d4472a6e59aafe636d626e955405e4369
druckhead <daniel32raz@gmail.com>
2022-12-14 22:52:20 +0200

    add Password exceptions

--- Lessons/Lesson_9/src/Homework/C1_Bus/exceptions.py
+++ Lessons/Lesson_9/src/Homework/C1_Bus/exceptions.py
@@ -1,8 +1,22 @@
 class BaseBusCompanyException(Exception):
     pass
 
+
 class MissingSearchKeyError(BaseBusCompanyException):
     pass
 
+
 class MissingRideIDError(BaseBusCompanyException):
+    pass
+
+
+class BasePasswordException(Exception):
+    pass
+
+
+class InvalidPasswordError(BasePasswordException):
+    pass
+
+
+class TooManyWrongPasswordAttemptsError(BasePasswordException):
     pass
