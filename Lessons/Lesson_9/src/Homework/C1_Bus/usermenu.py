517446e2a5e4eee93d961aec2bfef3e23f0a02d7
druckhead <daniel32raz@gmail.com>
2022-12-14 20:05:36 +0200

    add calls to inner bus_company functions

--- Lessons/Lesson_9/src/Homework/C1_Bus/usermenu.py
+++ Lessons/Lesson_9/src/Homework/C1_Bus/usermenu.py
@@ -1,4 +1,6 @@
 from src.Homework.C1_Bus.bestbuscompany import BestBusCompany
+from datetime import timedelta, datetime
+from uuid import uuid4
 
 
 class PassengerMenu:
@@ -6,12 +8,13 @@ class PassengerMenu:
         pass
 
     @staticmethod
-    def search_route(bus_company: BestBusCompany):
-        pass
+    def search_route(bus_company: BestBusCompany, line_num: str = None, origin: str = None, destination: str = None,
+                     bus_stop: str = None):
+        return bus_company.search_route(line_num, origin, destination, bus_stop)
 
     @staticmethod
-    def report_delay(bus_company: BestBusCompany):
-        pass
+    def report_delay(bus_company: BestBusCompany, line_num: str, ride_id: uuid4, delay: timedelta):
+        bus_company.report_delay(line_num, ride_id, delay)
 
 
 class ManagerMenu:
@@ -19,17 +22,19 @@ class ManagerMenu:
         pass
 
     @staticmethod
-    def add_route(bus_company: BestBusCompany):
-        pass
+    def add_route(bus_company: BestBusCompany, line_num: str, origin: str, destination: str, stops: list[str]):
+        bus_company.add_route(line_num, origin, destination, stops)
 
     @staticmethod
-    def delete_route(bus_company: BestBusCompany):
-        pass
+    def delete_route(bus_company: BestBusCompany, line_num: str):
+        bus_company.delete_route(line_num)
 
     @staticmethod
-    def update_route(bus_company: BestBusCompany):
-        pass
+    def update_route(bus_company: BestBusCompany, line_num: str = None, origin: str = None, destination: str = None,
+                     stops: list[str] = None):
+        bus_company.update_route(line_num, origin, destination, stops)
 
     @staticmethod
-    def add_scheduled_ride(bus_company: BestBusCompany):
-        pass
+    def add_scheduled_ride(bus_company: BestBusCompany, line_num: str, origin_time: datetime,
+                           destination_time: datetime, driver_name: str):
+        bus_company.add_scheduled_ride(line_num, origin_time, destination_time, driver_name)
