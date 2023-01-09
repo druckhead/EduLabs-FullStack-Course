ce4af87fddf959350d4553dbab1a0dad77de79e0
druckhead <daniel32raz@gmail.com>
2023-01-10 01:36:26 +0200

    add quota reached exception

--- Lessons/Lesson_16/Homework/virustotalapi/exceptions.py
+++ Lessons/Lesson_16/Homework/virustotalapi/exceptions.py
@@ -9,19 +9,30 @@ class VirusTotalExceptions(Exception):
 
 class BadRequest(VirusTotalExceptions):
     def __init__(self, response: Response) -> None:
-        super().__init__(f"Request received bad response from server\n"
-                         f"{response.status_code}: {response.text}"
-                         )
-        
+        super().__init__(
+            f"Request received bad response from server\n"
+            f"{response.status_code}: {response.text}"
+        )
+
+
 class AnalysisDataDoesNotExist(VirusTotalExceptions):
     def __init__(self, url: str) -> None:
         super().__init__(f"No analysis found for: {url}")
 
 
 class AnalysisExpired(VirusTotalExceptions):
-    def __init__(self, url: str, last_analysis: datetime, expire_date: datetime) -> None:
-        super().__init__(f"Cache for: {url} expired. "
-                         f"{abs((last_analysis - expire_date).days)} days ago.\n"
-                         f"Last analysis date: {last_analysis}\n"
-                         f"Expire date: {expire_date}"
-                         )
+    def __init__(
+        self, url: str, last_analysis: datetime, expire_date: datetime
+    ) -> None:
+        super().__init__(
+            f"Cache for: {url} expired. "
+            f"{abs((last_analysis - expire_date).days)} days ago.\n"
+            f"Last analysis date: {last_analysis}\n"
+            f"Expire date: {expire_date}"
+        )
+
+
+class QuoataReachedError(VirusTotalExceptions):
+    def __init__(self, response: Response) -> None:
+        super().__init__(f"{response.json().get('error').get('message')}\n"
+                         f"Try again later.")
