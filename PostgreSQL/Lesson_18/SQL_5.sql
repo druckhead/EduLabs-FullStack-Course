a0f0fb895e5384079e3757e2a7411620e6bbe5f7
druckhead <daniel32raz@gmail.com>
2023-01-11 22:03:45 +0200

    finished queries

--- PostgreSQL/Lesson_18/SQL_5.sql
+++ PostgreSQL/Lesson_18/SQL_5.sql
@@ -46,9 +46,44 @@ WHERE rs.stop_id = (
             AND s.name = 'Matam'
     );
 
-
 -- Get driver names and amount of routes they drove in the last year
+SELECT d.name,
+    COUNT(r2.id) AS num_rides
+FROM drivers d
+    LEFT JOIN rides r1 ON d.id = r1.driver_id
+    LEFT JOIN routes r2 ON r1.route_id = r2.id
+GROUP BY d.name;
+
 -- Display driver names and total time they drove. Note, you should not take into account canceled rides. 
+SELECT d.name,
+    sum(r2.arrival_time - r2.departure_time) len_rides
+FROM drivers d
+    LEFT JOIN rides r1 ON d.id = r1.driver_id
+    LEFT JOIN ride_stops r2 ON r1.route_id = r2.id
+GROUP BY d.name,
+    r2.arrival_time - r2.departure_time;
+
 -- Get all the driver names who drive on route 100
+SELECT d.name
+FROM drivers d
+    LEFT JOIN rides r1 ON r1.driver_id = d.id
+    LEFT JOIN routes r2 ON r2.id = r1.route_id
+WHERE r2.route_num = 100;
+
 -- Get all the routes which rides last more than 1 hour
+SELECT rs1.*
+FROM rides r1
+    LEFT JOIN routes r2 ON r2.id = r1.route_id
+    LEFT JOIN ride_stops rs1 ON rs1.id = r1.id
+WHERE (rs1.arrival_time - rs1.arrival_time) > INTERVAL '1' HOUR;
+
 -- Display driver names, and amount of delays for each in the rides they drove
+SELECT d.name AS driver_name,
+    r1.id AS ride_id,
+    COUNT(si1.id) AS amount_of_delays
+FROM drivers d
+    LEFT JOIN rides r1 ON d.id = r1.driver_id
+    LEFT JOIN service_interruptions si1 ON r1.id = si1.ride_id
+WHERE si1.interrupt_type = 'delay'
+GROUP BY d.name,
+    r1.id;
