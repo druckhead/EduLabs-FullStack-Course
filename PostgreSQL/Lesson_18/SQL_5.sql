38b6ac40b8c21d1b3b950ed6e5412c99bf62359d
druckhead <daniel32raz@gmail.com>
2023-01-11 13:19:09 +0200

    add two more queries

--- PostgreSQL/Lesson_18/SQL_5.sql
+++ PostgreSQL/Lesson_18/SQL_5.sql
@@ -11,7 +11,8 @@ SELECT r1.route_num AS route_id,
     COUNT(r2.id) AS amnt_ride_id
 FROM routes r1
     LEFT JOIN rides r2 ON r1.id = r2.route_id
-GROUP BY r1.route_num, r2.weekday;
+GROUP BY r1.route_num,
+    r2.weekday;
 
 -- display route with most rides
 SELECT r1.route_num,
@@ -23,7 +24,29 @@ ORDER BY COUNT(r2.id) DESC
 LIMIT 1;
 
 -- get the weekday that has most rides for route 200
+SELECT r2.weekday,
+    r1.route_num AS route_id,
+    COUNT(r2.id) AS amnt_ride_id
+FROM routes r1
+    LEFT JOIN rides r2 ON r1.id = r2.route_id
+WHERE r1.route_num = 200
+GROUP BY r1.route_num,
+    r2.weekday
+ORDER BY COUNT(r2.id) DESC
+LIMIT 1;
+
 -- Get all the routes that have stops in Haifa Matam
+SELECT *
+FROM ride_stops rs
+WHERE rs.stop_id = (
+        SELECT s.id
+        FROM stops s
+        GROUP BY s.id
+        HAVING s.city = 'Haifa'
+            AND s.name = 'Matam'
+    );
+
+
 -- Get driver names and amount of routes they drove in the last year
 -- Display driver names and total time they drove. Note, you should not take into account canceled rides. 
 -- Get all the driver names who drive on route 100
