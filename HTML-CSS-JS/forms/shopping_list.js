b59bc718e130c032d84fe3b04a54d6b60cffade1
druckhead <daniel32raz@gmail.com>
2023-02-12 15:07:58 +0200

    fixes

--- HTML-CSS-JS/forms/shopping_list.js
+++ HTML-CSS-JS/forms/shopping_list.js
@@ -10,8 +10,11 @@ function formSubmitHandler(event) {
     deleteButton.setAttribute('class', 'btn btn-outline-danger form-control-sm');
     deleteButton.innerHTML = "X";
     deleteButton.addEventListener("click", (event) => {
-        const deleteTarget = event.target.parentElement.parentElement
-        deleteTarget.remove();
+        const isConfirmed = confirm('Are you sure?')
+        if (isConfirmed) {
+            const deleteTarget = event.target.parentElement.parentElement
+            deleteTarget.remove();
+        }
     });
 
     const addButton = document.createElement('button');
@@ -42,36 +45,55 @@ function formSubmitHandler(event) {
     const list = document.getElementById('products');
     const fd = new FormData(event.target);
 
+    let exists = false
+    let tagElement = null
+
     for (const pair of fd.entries()) {
         if (pair[0] === 'product') {
-            const p = document.createElement('p');
-            p.setAttribute("class", "my-auto text-white")
-            p.innerHTML = pair[1];
-            productItem.appendChild(p);
-            item.appendChild(productItem);
-            list.appendChild(item);
+            for (const item of Array.from(list.children).filter(item => item.tagName.toLowerCase() === 'li')) {
+                const tag = item.children[0].children[0]
+                if (tag.innerText === pair[1]) {
+                    exists = true
+                    tagElement = tag
+                    break
+                }
+            }
+            if (!exists) {
+                const p = document.createElement('p');
+                p.setAttribute("class", "my-auto text-white")
+                p.innerHTML = pair[1];
+                productItem.appendChild(p);
+                item.appendChild(productItem);
+                list.appendChild(item);
+            }
         } else if (pair[0] === 'product-amnt') {
-            const span = document.createElement('span');
-            span.setAttribute("class", "my-auto badge text-bg-secondary");
-            span.innerHTML = pair[1];
-            productItem.appendChild(span);
-            item.appendChild(productItem);
-            list.appendChild(item);
-            item.appendChild(btnContainer);
+            if (exists) {
+                tagElement.parentElement.children[1].innerText = Number(tagElement.parentElement.children[1].innerText) + Number(pair[1])
+            } else {
+                const span = document.createElement('span');
+                span.setAttribute("class", "my-auto badge text-bg-secondary");
+                span.innerHTML = pair[1];
+                productItem.appendChild(span);
+                item.appendChild(productItem);
+                list.appendChild(item);
+                item.appendChild(btnContainer);
+            }
         }
     }
     event.target.reset()
 }
 
 function clearAll() {
-    const list = document.getElementById('products');
-    let toRemove = [];
-    for (const item of list.children) {
-        if (item.tagName.toLowerCase() === 'li') {
-            toRemove.push(item);
+    if (isConfirmed) {
+        const list = document.getElementById('products');
+        let toRemove = [];
+        for (const item of list.children) {
+            if (item.tagName.toLowerCase() === 'li') {
+                toRemove.push(item);
+            }
         }
-    }
-    for (const item of toRemove) {
-        item.remove()
+        for (const item of toRemove) {
+            item.remove()
+        }
     }
 }
